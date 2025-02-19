051a7cdbd74ccb9333bbbbeee604c05689122358
LucaGemma <42171162+LucaGemma@users.noreply.github.com>
2025-02-19 17:20:18 +0100

    Update main.py
    Added a second variable

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ x = 10
 
 # Visualizzo il suo valore
 print(f"Il valore di x è {x}")
+
+# Inizializzo una seconda variabile al doppio di x
+y = 2*x
+
+# Visualizzo il suo valore
+print(f"Il valore di y è {y}")
