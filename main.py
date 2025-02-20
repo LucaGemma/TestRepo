2ea34d2e9a38662fd59346e038c7dbd584d32505
LucaGemma <luca.gemma.91@gmail.com>
2025-02-20 09:10:04 +0100

    Added Github exercise to main.py
    Added a short exercise to make some practice with Github to main.py

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ y = 2*x
 
 # Visualizzo il suo valore
 print(f"Il valore di y è {y}")
+
+# TODO:
+# Da GitHub.com
+# Creare una nuova Repo pubblica su Github
+# Riempire il README.md con una breve descrizione del progetto e fare commit sul main
+# Creare un nuovo file main.py e fare commit su un branch "develop"
+# Inserire tre istruzioni Python valide e fare commit sul branch "develop"
+# Fare un merge del branch "develop" sul branch "main"
+
+# Da Github Desktop / VSCode / PyCharm:
+# Fare un clone della Repo
+# Modificare il file main.py aggiungendo altre tre istruzioni Python valide e fare commit sul main
+# Fare push delle modifiche sul repository remoto
+
+# Da Github.com:
+# Aggiungere un file .txt (crearlo o caricarlo) e fare commit sul main
+
+# Da Github Desktop / VSCode / PyCharm:
+# Fare pull delle modifiche dal repository remoto
+# Modificare il file main.py in modo che aggiunga la stringa di testo "Hello world!" al file .txt e fare commit sul main
+# Fare push delle modifiche sul repository remoto
+
+# Da Github.com:
+# Verificare che il file .txt sia stato modificato correttamente
